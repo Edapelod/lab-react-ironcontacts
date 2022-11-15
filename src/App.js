fdd569a6b380dd48ff6e0f24c81495892b6e5615
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-15 15:33:31 +0100

    iteration1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import contacts from "./contacts.json";
+import "./App.css";
 function App() {
+  const [contactsList, setContactsList] = useState(contacts.slice(0, 5));
+  if (contactsList) {
+    console.log(contactsList);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <h2>Picture</h2>
+            </th>
+            <th>
+              <h2>Name</h2>
+            </th>
+            <th>
+              <h2>Popularity</h2>
+            </th>
+            <th>
+              <h2>Won an Oscar</h2>
+            </th>
+            <th>
+              <h2>Won an Emmy</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsList.map((celebrity) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    style={{ height: "200px" }}
+                    src={celebrity.pictureUrl}
+                    alt="celebrity"
+                  />
+                </td>
+                <td>
+                  <h4>{celebrity.name}</h4>
+                </td>
+                <td>
+                  <h4>{celebrity.popularity}</h4>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
