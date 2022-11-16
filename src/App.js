96e80d11114abf0fd36e0d7e1e92acb11ed074c3
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-16 09:16:23 +0100

    iteration3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import contacts from "./contacts.json";
 import "./App.css";
 function App() {
   const [contactsList, setContactsList] = useState(contacts.slice(0, 20));
+  const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+  // check if there is a contact list
   if (contactsList) {
     console.log(contactsList);
   }
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      {/* <button onClick={toggleShowMovies}>{showMovies ? "Hide" : "Show"}</button> */}
       <table>
         <thead>
           <tr>
@@ -32,27 +35,29 @@ function App() {
         <tbody>
           {contactsList.map((celebrity) => {
             return (
-              <tr>
-                <td>
-                  <img
-                    style={{ height: "200px" }}
-                    src={celebrity.pictureUrl}
-                    alt="celebrity"
-                  />
-                </td>
-                <td>
-                  <h4>{celebrity.name}</h4>
-                </td>
-                <td>
-                  <h4>{celebrity.popularity}</h4>
-                </td>
-                <td>
-                  <h4>{celebrity.wonOscar ? "🏆" : ""}</h4>
-                </td>
-                <td>
-                  <h4>{celebrity.wonEmmy ? "🏆" : ""}</h4>
-                </td>
-              </tr>
+              <div key={celebrity.id}>
+                <tr>
+                  <td>
+                    <img
+                      style={{ height: "200px" }}
+                      src={celebrity.pictureUrl}
+                      alt="celebrity"
+                    />
+                  </td>
+                  <td>
+                    <h4>{celebrity.name}</h4>
+                  </td>
+                  <td>
+                    <h4>{celebrity.popularity}</h4>
+                  </td>
+                  <td>
+                    <h4>{celebrity.wonOscar ? "🏆" : ""}</h4>
+                  </td>
+                  <td>
+                    <h4>{celebrity.wonEmmy ? "🏆" : ""}</h4>
+                  </td>
+                </tr>
+              </div>
             );
           })}
         </tbody>
